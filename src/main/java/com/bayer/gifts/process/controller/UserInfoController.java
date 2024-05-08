fa1b207d86b68afe813ce31a20feee196101a37f
Sands-Sun <zhe.sun.ext@bayer.com>
2024-05-08 17:55:25 +0800

    merge from sso

--- src/main/java/com/bayer/gifts/process/controller/UserInfoController.java
+++ src/main/java/com/bayer/gifts/process/controller/UserInfoController.java
@@ -12,11 +12,7 @@ import com.bayer.gifts.process.sys.service.RouterService;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -37,9 +33,9 @@ public class UserInfoController extends AbstractController{
     @ApiOperation("根据Id获得用户信息")
     @RequestMapping(value = "/get/{id}", method = RequestMethod.GET)
     public R<UserExtensionEntity> get(@PathVariable Long id,
-                                      @RequestParam(value = "includeRole",
-                                              defaultValue = "false") boolean includeRole) {
-        return R.ok(userInfoService.getUserInfo(id,includeRole));
+                                      @RequestParam(value = "includeRole", defaultValue = "false") boolean includeRole,
+                                      @RequestParam(value = "includeGroup", defaultValue = "false") boolean includeGroup) {
+        return R.ok(userInfoService.getUserInfo(id,includeRole,includeGroup));
     }
 
     @ApiOperation("获得用户信息")
